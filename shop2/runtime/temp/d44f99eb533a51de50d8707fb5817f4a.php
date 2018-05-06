118d9be234f2ea334fc40737cf7a95833ee24711
tjlywhg2001 <tjlywhg=2001@163.com>
2018-05-06 12:09:35 +0800

    20180506-111

--- shop2/runtime/temp/d44f99eb533a51de50d8707fb5817f4a.php
+++ shop2/runtime/temp/d44f99eb533a51de50d8707fb5817f4a.php
@@ -1,15 +1,247 @@
+<?php if (!defined('THINK_PATH')) exit(); /*a:5:{s:79:"G:\github\dese001\shop2\public/../application/admin\view\config\configlist.html";i:1525567301;s:64:"G:\github\dese001\shop2\application\admin\view\common\_meta.html";i:1525566620;s:62:"G:\github\dese001\shop2\application\admin\view\common\top.html";i:1525052181;s:63:"G:\github\dese001\shop2\application\admin\view\common\left.html";i:1525433586;s:65:"G:\github\dese001\shop2\application\admin\view\common\footer.html";i:1525566627;}*/ ?>
 <!DOCTYPE html>
 <html>
-{include file='/common/_meta'}
+<head>
+	    <meta charset="utf-8">
+    <title>童老师ThinkPHP交流群：484519446</title>
+
+    <meta name="description" content="Dashboard">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <!--Basic Styles-->
+    <link href="/static/admin/style/bootstrap.css" rel="stylesheet">
+    <link href="/static/admin/style/font-awesome.css" rel="stylesheet">
+    <link href="/static/admin/style/weather-icons.css" rel="stylesheet">
+
+    <!--Beyond styles-->
+    <link id="beyond-link" href="/static/admin/style/beyond.css" rel="stylesheet" type="text/css">
+    <link href="/static/admin/style/demo.css" rel="stylesheet">
+    <link href="/static/admin/style/typicons.css" rel="stylesheet">
+    <link href="/static/admin/style/animate.css" rel="stylesheet">
+    <script src="/static/plus/ueditor/ueditor.config.js"></script>
+    <script src="/static/plus/ueditor/ueditor.all.min.js"></script>
+    <script src="/static/plus/ueditor/lang/zh-cn/zh-cn.js"></script>
+    <style>
+        .asdf{background: #559;}
+        .page-sidebar::before{left: 0px; width: 224px;}
+    </style>
+    
+</head>
+
 <body>
 	<!-- 头部 -->
-{include file='/common/top'}
+	<div class="navbar">
+    <div class="navbar-inner">
+        <div class="navbar-container">
+            <!-- Navbar Barnd -->
+            <div class="navbar-header pull-left">
+                <a href="#" class="navbar-brand">
+                    <small>
+                            <img src="/static/admin/images/logo.png" alt="">
+                        </small>
+                </a>
+            </div>
+            <!-- /Navbar Barnd -->
+            <!-- Sidebar Collapse -->
+            <div class="sidebar-collapse" id="sidebar-collapse">
+                <i class="collapse-icon fa fa-bars"></i>
+            </div>
+            <!-- /Sidebar Collapse -->
+            <!-- Account Area and Settings -->
+            <div class="navbar-header pull-right">
+                <div class="navbar-account">
+                    <ul class="account-area">
+                        <li>
+                            <a class="login-area dropdown-toggle" data-toggle="dropdown">
+                                <div class="avatar" title="View your public profile">
+                                    <img src="/static/admin/images/adam-jansen.jpg">
+                                </div>
+                                <section>
+                                    <h2><span class="profile"><span>admin</span></span></h2>
+                                </section>
+                            </a>
+                            <!--Login Area Dropdown-->
+                            <ul class="pull-right dropdown-menu dropdown-arrow dropdown-login-area">
+                                <li class="username"><a>David Stevenson</a></li>
+                                <li class="dropdown-footer">
+                                    <a href="/admin/user/logout.html">
+                                            退出登录
+                                        </a>
+                                </li>
+                                <li class="dropdown-footer">
+                                    <a href="/admin/user/changePwd.html">
+                                            修改密码
+                                        </a>
+                                </li>
+                            </ul>
+                            <!--/Login Area Dropdown-->
+                        </li>
+                        <!-- /Account Area -->
+                        <!--Note: notice that setting div must start right after account area list.
+                            no space must be between these elements-->
+                        <!-- Settings -->
+                    </ul>
+                </div>
+            </div>
+            <!-- /Account Area and Settings -->
+        </div>
+    </div>
+</div>
+
 	<!-- /头部 -->
 	
 	<div class="main-container container-fluid">
 		<div class="page-container">
 			            <!-- Page Sidebar -->
-        {include file='/common/left'}
+        <div class="page-sidebar" id="sidebar">
+<!-- Page Sidebar Header-->
+<div class="sidebar-header-wrapper">
+    <input class="searchinput" type="text">
+    <i class="searchicon fa fa-search"></i>
+    <div class="searchhelper">Search Reports, Charts, Emails or Notifications</div>
+</div>
+<!-- /Page Sidebar Header -->
+<!-- Sidebar Menu -->
+<style>
+ul>li{
+    box-shadow: none;
+}
+</style>
+<ul class="nav sidebar-menu">
+    <!--Dashboard-->
+<!--     <li>
+        <a href="#">
+            <i class="menu-icon fa fa-film"></i>
+            <span class="menu-text">ThinkPHP5视频教程</span>
+            <i class="menu-expand"></i>
+        </a>
+    </li>
+    <li>
+        <a href="#">
+            <i class="menu-icon fa fa-gear"></i>
+            <span class="menu-text">控制面板</span>
+            <i class="menu-expand"></i>
+        </a>
+    </li>                        
+ -->    <li>
+        <a href="#" class="menu-dropdown">
+            <i class="menu-icon fa fa-gear"></i>
+            <span class="menu-text">栏目模块</span>
+            <i class="menu-expand"></i>
+        </a>
+        <ul class="submenu"  style="display: block;">
+            <li>
+                <a href="<?php echo url('Category/lst'); ?>">
+                    <span class="menu-text">栏目管理</span>
+                    <i class="menu-expand"></i>
+                </a>
+            </li>
+        </ul>
+    </li>
+    <li>
+        <a href="#" class="menu-dropdown">
+            <i class="menu-icon fa fa-gear"></i>
+            <span class="menu-text">品牌模块</span>
+            <i class="menu-expand"></i>
+        </a>
+        <ul class="submenu"  style="display: block;">
+            <li>
+                <a href="<?php echo url('brand/lst'); ?>">
+                    <span class="menu-text">品牌管理</span>
+                    <i class="menu-expand"></i>
+                </a>
+            </li>
+        </ul>
+    </li>
+    <li>
+        <a href="#" class="menu-dropdown">
+            <i class="menu-icon fa fa-gear"></i>
+            <span class="menu-text">文章模块</span>
+            <i class="menu-expand"></i>
+        </a>
+        <ul class="submenu"  style="display: block;">
+            <li>
+                <a href="<?php echo url('arti/lst'); ?>">
+                    <span class="menu-text">文章管理</span>
+                    <i class="menu-expand"></i>
+                </a>
+            </li>
+            <li>
+                <a href="<?php echo url('arti/imglist'); ?>">
+                    <span class="menu-text">图片管理</span>
+                    <i class="menu-expand"></i>
+                </a>
+            </li>
+        </ul>
+    </li>
+    <li>
+        <a href="#" class="menu-dropdown">
+            <i class="menu-icon fa fa-gear"></i>
+            <span class="menu-text">链接模块</span>
+            <i class="menu-expand"></i>
+        </a>
+        <ul class="submenu"  style="display: block;">
+            <li>
+                <a href="<?php echo url('link/lst'); ?>">
+                    <span class="menu-text">链接管理</span>
+                    <i class="menu-expand"></i>
+                </a>
+            </li>
+        </ul>
+    </li>
+    <li>
+        <a href="#" class="menu-dropdown">
+            <i class="menu-icon fa fa-gear"></i>
+            <span class="menu-text">配置模块</span>
+            <i class="menu-expand"></i>
+        </a>
+        <ul class="submenu"  style="display: block;">
+            <li>
+                <a href="<?php echo url('config/configlst'); ?>">
+                    <span class="menu-text">配置项</span>
+                    <i class="menu-expand"></i>
+                </a>
+            </li>
+            <li>
+                <a href="<?php echo url('config/lst'); ?>">
+                    <span class="menu-text">配置管理</span>
+                    <i class="menu-expand"></i>
+                </a>
+            </li>
+        </ul>
+    </li>
+<!--     <li>
+        <a href="#" class="menu-dropdown">
+            <i class="menu-icon fa fa-gear"></i>
+            <span class="menu-text">系统</span>
+            <i class="menu-expand"></i>
+        </a>
+        <ul class="submenu" style="display: block;">
+            <li>
+                <a href="">
+                    <span class="menu-text">用户管理</span>
+                    <i class="menu-expand"></i>
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <span class="menu-text">角色管理</span>
+                    <i class="menu-expand"></i>
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <span class="menu-text">权限列表</span>
+                    <i class="menu-expand"></i>
+                </a>
+            </li>
+        </ul>
+    </li>                        
+ --></ul>
+<!-- /Sidebar Menu -->
+</div>
+
             <!-- /Page Sidebar -->
             <!-- Page Content -->
             <div class="page-content">
@@ -17,10 +249,10 @@
                 <div class="page-breadcrumbs">
                     <ul class="breadcrumb">
                         <li>
-                            <a href="{:url('Index/index')}">系统</a>
+                            <a href="<?php echo url('Index/index'); ?>">系统</a>
                         </li>
                                             <li>
-                            <a href="{:url('config/lst')}">配置管理</a>
+                            <a href="<?php echo url('config/lst'); ?>">配置管理</a>
                         </li>
                         <li class="active">配置项</li>
                     </ul>
@@ -108,12 +340,10 @@
                                             <input id="username" name="config_cname" type="file">
 
                                                 <?php if($confs['config_default']): ?>
-                                                    <img src="__upload__/{$confs.confing_default}" alt="" height="40">
+                                                    <img src="/static/uploadss/<?php echo $confs['confing_default']; ?>" alt="" height="40">
                                                 <?php else: ?>
                                                     <p style="line-height: 40px">暂无图片</p>
-                                                <?php endif; ?>
-
-                                            <?php endif; ?>
+                                                <?php endif; endif; ?>
 
                                         </div>
                                     </div>
@@ -198,7 +428,20 @@
 	</div>
 
 	    <!--Basic Scripts-->
-    {include file='/common/footer'}
+        <script src="/static/admin/style/jquery_002.js"></script>
+    <script src="/static/admin/style/bootstrap.js"></script>
+    <script src="/static/admin/style/jquery.js"></script>
+    <!--Beyond Scripts-->
+    <script src="/static/admin/style/beyond.js"></script>
+	<script type="text/javascript">
+
+    //实例化编辑器
+    //建议使用工厂方法getEditor创建和引用编辑器实例，如果在某个闭包下引用该编辑器，直接调用UE.getEditor('editor')就能拿到相关的实例
+    UE.getEditor('content',{initialFrameWidth:795,initialFrameHeight:300,});
+    
+
+
+</script>
     
 
 </body></html>
